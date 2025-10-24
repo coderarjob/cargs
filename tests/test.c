9006daa635c36ef15c8f203feabcbc3c9b256200
Arjob Mukherjee <arjobmukherjee@gmail.com>
2025-10-24 09:09:13 +0530

    Calculated argc with ARRAY_LEN macro

--- tests/test.c
+++ tests/test.c
@@ -60,13 +60,12 @@ YT_TESTP (cargs, required_arguments, bool, bool)
 YT_TEST (cargs, list_type_argument_success)
 {
     char* argv[] = { "dummy", "-A", "abc", "-B", "1", "2", "3", "-C", "true", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     char* a            = cargs_add_arg ("A", "1st arg", String, NULL);
     Cargs_ArrayList* b = cargs_add_arg ("B", "2nd arg", CARGS_LISTOF (Integer), NULL);
     char* c            = cargs_add_arg ("C", "3rd arg", Boolean, NULL);
 
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
 
     YT_EQ_STRING (a, "abc");
     YT_EQ_SCALAR (*c, true);
@@ -81,10 +80,9 @@ YT_TEST (cargs, list_type_argument_success)
 YT_TEST (cargs, arg_value_length_clamping)
 {
     char* argv[] = { "dummy", "-A", "123456789ABCD", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     char* a = cargs_add_arg ("A", "1st arg", String, NULL);
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
 
     // CARGS_MAX_INPUT_VALUE_LEN_OVERRIDE number of non-null characters
     YT_EQ_STRING (a, "123456789A");
@@ -97,9 +95,8 @@ YT_TEST (cargs, arg_name_length_clamping)
 
     // Argument name is clamped to CARGS__MAX_NAME_LEN_OVERRIDE number of non-null characters
     char* argv[] = { "dummy", "-A23456789BCD", "123", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
     YT_EQ_STRING (a, "123");
 
     YT_END();
@@ -112,7 +109,6 @@ YT_TEST (cargs, help_argument_present)
     // but has one Help arg and as a result cargs_parse_input would pass this time.
 
     char* argv[] = { "dummy", "-A", "abc", "-C", "true", "-D", "12.84", "-H", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     cargs_add_arg ("A", "1st arg", String, NULL);
     cargs_add_arg ("B", "2nd arg", Integer, NULL);
@@ -120,7 +116,7 @@ YT_TEST (cargs, help_argument_present)
     cargs_add_arg ("D", "4th arg", Double, NULL);
     cargs_add_arg ("H", "Help arg", Help, "false");
 
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
 
     YT_END();
 }
@@ -128,14 +124,13 @@ YT_TEST (cargs, help_argument_present)
 YT_TEST (cargs, default_values)
 {
     char* argv[] = { "dummy", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     char* a   = cargs_add_arg ("A", "1st arg", String, "bcd");
     int* b    = cargs_add_arg ("B", "2nd arg", Integer, "13");
     bool* c   = cargs_add_arg ("C", "3rd arg", Boolean, "false");
     double* d = cargs_add_arg ("D", "4th arg", Double, "18.52");
 
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
 
     YT_EQ_STRING (a, "bcd");
     YT_EQ_SCALAR (*b, 13);
@@ -148,14 +143,13 @@ YT_TEST (cargs, default_values)
 YT_TEST (cargs, default_value_override)
 {
     char* argv[] = { "dummy", "-A", "def", "-B", "23", "-C", "true", "-D", "220.72", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     char* a   = cargs_add_arg ("A", "1st arg", String, "bcd");
     int* b    = cargs_add_arg ("B", "2nd arg", Integer, "13");
     bool* c   = cargs_add_arg ("C", "3rd arg", Boolean, "false");
     double* d = cargs_add_arg ("D", "4th arg", Double, "18.52");
 
-    YT_EQ_SCALAR (true, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
 
     YT_EQ_STRING (a, "def");
     YT_EQ_SCALAR (*b, 23);
@@ -167,11 +161,13 @@ YT_TEST (cargs, default_value_override)
 YT_TEST (cargs, unknown_argument_in_cl)
 {
     char* argv[] = { "dummy", "-B", "23", NULL };
-    int argc     = sizeof (argv) / sizeof (argv[0]);
 
     cargs_add_arg ("A", "1st arg", String, NULL);
 
-    YT_EQ_SCALAR (false, cargs_parse_input (argc, argv));
+    YT_EQ_SCALAR (false, cargs_parse_input (ARRAY_LEN (argv), argv));
+
+    YT_END();
+}
 YT_TESTP (cargs, inactive_argument_in_cl, bool)
 {
     bool is_inactive_arg_present_in_cl = YT_ARG_0();
@@ -183,10 +179,10 @@ YT_TESTP (cargs, inactive_argument_in_cl, bool)
 
     if (is_inactive_arg_present_in_cl) {
         char* argv[] = { "dummy", "-B", NULL };
-        YT_EQ_SCALAR (false, cargs_parse_input (ARRAY_LEN(argv), argv));
+        YT_EQ_SCALAR (false, cargs_parse_input (ARRAY_LEN (argv), argv));
     } else {
         char* argv[] = { "dummy", NULL };
-        YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN(argv), argv));
+        YT_EQ_SCALAR (true, cargs_parse_input (ARRAY_LEN (argv), argv));
     }
 
     YT_END();
